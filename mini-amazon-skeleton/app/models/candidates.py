e0ad7a40a7069aff0eb53a6d9ebd8b20bbb0f273
Shravan Kalahasthy <svk3@duke.edu>
2022-10-23 16:42:01 -0400

    Added links so user can click a candidate's name and see voting history

--- mini-amazon-skeleton/app/models/candidates.py
+++ mini-amazon-skeleton/app/models/candidates.py
@@ -17,6 +17,7 @@ class Candidate_Vote:
         SELECT candidate_name, candidate_vote, vote_date, vote_description, vote_result
         FROM Candidate_Vote
         WHERE id = :cid
+        ORDER BY vote_date
         ''',
                               cid=cid)
         if len(rows) > 0:
@@ -26,7 +27,8 @@ class Candidate_Vote:
     def get_all_candidates():
         rows = app.db.execute('''
         SELECT DISTINCT c.id, c.candidate_name
-        FROM Candidate_Vote c''')
+        FROM Candidate_Vote c
+        ORDER BY c.candidate_name''')
         if len(rows) > 0:
             return rows
         return "oops"
