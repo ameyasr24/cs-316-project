92d3f3ead3f47543acc2eb7a9e50031b05cb0ba4
Michelle Qiu <mq37@duke.edu>
2022-11-09 22:56:04 -0500

    added some features

--- mini-amazon-skeleton/app/committees.py
+++ mini-amazon-skeleton/app/committees.py
@@ -1,69 +1,100 @@
 from flask import render_template,flash
 import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField, SelectField,SelectMultipleField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
+from wtforms import BooleanField as WTBool
 from .models.committees import Committees
 
 from flask import Blueprint
 bp = Blueprint('committees', __name__)
 
-
-class SearchCommittee(FlaskForm):
-    query = SelectField('query',choices=['all donations involving','donations to/from'],default='all donations involving')
-    any_ent = StringField('Entity')
-    to_ent = StringField('To entity')
-    from_ent = StringField('From entity')
+class SearchFirst (FlaskForm):
+    query = SelectField('query',choices=['all donations','donations involving','donations to/from'],default='all donations')
+    search = SubmitField('Select')
+class AllCommittees(FlaskForm):
     from_year = IntegerField('From Year',default=0)
     to_year=IntegerField('To Year',default=2022)
-   # order_by=SelectField('Order by:',choices=['cid', 'date ascending','date descending'],default='cid')
+    order_by=SelectField('Order by:',choices=['ID', 'year','donation amount'],default='ID')
+    sort=SelectField(choices=['ascending','descending'],default='ascending')
+    query = SelectField('query',choices=['all donations', 'donations involving','donations to/from'],default='all donations')
     search = SubmitField('Search')
-
-# original committee without the form function in the html file
-# @bp.route('/committee', methods=['GET', 'POST'])
-# def committees():
-#     comms = Committees.get_all()
-#     return render_template('committees.html', 
-#                            all_committees=comms)
-
+   # total = SelectMultipleField('Total Sum',choices=['Find total sum'])
+    #reset = SubmitField('Reset')
+class SearchCommitteeInvolving(FlaskForm):
+    from_year = IntegerField('From Year',default=0)
+    to_year=IntegerField('To Year',default=2022)
+    order_by=SelectField('Order by:',choices=['ID', 'year','donation amount'],default='ID')
+    sort=SelectField(choices=['ascending','descending'],default='ascending')
+    query = SelectField('query',choices=['all donations', 'donations involving','donations to/from'],default='all donations')
+    any_ent = StringField('Entity')
+    search = SubmitField('Search')
+    #total = SelectMultipleField('Total Sum',choices=[])
+class SearchCommitteeToFrom(FlaskForm):
+    from_year = IntegerField('From Year',default=0)
+    to_year=IntegerField('To Year',default=2022)
+    order_by=SelectField('Order by:',choices=['ID', 'year','donation amount'],default='ID')
+    sort=SelectField(choices=['ascending','descending'],default='ascending')
+    query = SelectField('query',choices=['all donations','donations involving','donations to/from'],default='all donations')
+    to_ent = StringField('To entity')
+    from_ent = StringField('From entity')
+    search = SubmitField('Search')
+   # total = SelectMultipleField('Total Sum',choices=[])
 
 @bp.route('/committee', methods=['GET', 'POST'])
 def committees():
-    #to do: if there is no to entity provided, get all from from_entity from given times/default
-    # if there is not from entity provided, get all from to_entity from given times/default
-    #if both, then get all entities from given times/default
-    
-    form = SearchCommittee()
-    if form.query =='all donations involving':
-        return render_template('committees.html', form=form,involving=True)
-    elif form.query=='donations to/from':
-        return render_template('committees.html',form=form,toFrom=True)
-    searchedcomms=Committees.get_all()
+    output=[]
+    form = SearchFirst()
+    type_form=''
+    searchedcomms=Committees.get_all('ID','ascending')
     if form.validate_on_submit():
-        if form.to_ent.data or form.from_ent.data:
-            if form.to_ent.data and form.from_ent.data:
-                flash('You searched for donations from ' + form.from_ent.data + ' to ' + form.to_ent.data)
-                flash('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
-                searchedcomms=Committees.get_all_from_to_range(form.to_ent.data, form.from_ent.data, form.from_year.data,form.to_year.data)
-            elif form.to_ent.data:
-                flash('You searched for donations to ' + form.to_ent.data)
-                flash('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
-                searchedcomms=Committees.get_all_to_entity_range(form.to_ent.data, form.from_year.data,form.to_year.data)
-            elif form.from_ent.data: 
-                flash('You searched for donations from ' + form.from_ent.data)
-                flash('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
-                searchedcomms=Committees.get_all_from_entity_range(form.from_ent.data, form.from_year.data,form.to_year.data)
-        else:
-            flash('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
-            searchedcomms=Committees.get_all_range(form.from_year.data,form.to_year.data)
-    #    flash('Sorted by:' + form.order_by.data)
-     #   if form.order_by.data=='date ascending':
-      #      searchedcomms=Committees.sort_by_date_asc(searchedcomms)
-      #  elif form.order_by.data=='date descending':
-       #     searchedcomms=Committees.sort_by_date_desc(searchedcomms)
-    return render_template('committees.html', form=form,all_committees=searchedcomms)
+        if form.query.data=='donations involving':
+            type_form=form.query.data
+            form = SearchCommitteeInvolving()
+            helper(type_form,form)            
+        elif form.query.data=='donations to/from':
+            type_form=form.query.data
+            form=SearchCommitteeToFrom()
+            helper(type_form,form)
+        elif form.query.data=='all donations':
+            type_form=form.query.data
+            form=AllCommittees()
+            helper(type_form,form)
+            
 
+    #searchedcomms=Committees.get_all_range(form.from_year.data,form.to_year.data,form.order_by.data,form.sort.data)
+    if form.validate_on_submit():
+        if type_form=='donations involving':
+            output.append('You searched for donations involving ' + form.any_ent.data)
+            output.append('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
+            #flash('You searched for donations involving ' + form.any_ent.data )
+            #flash('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
+            searchedcomms=Committees.get_all_involving(form.any_ent.data, form.from_year.data,form.to_year.data,form.order_by.data,form.sort.data)
+        elif type_form=='donations to/from':
+            if form.to_ent.data and form.from_ent.data:
+                output.append('You searched for donations from ' + form.from_ent.data + ' to ' + form.to_ent.data)
+                output.append('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
+                searchedcomms=Committees.get_all_from_to_range(form.to_ent.data, form.from_ent.data, form.from_year.data,form.to_year.data,form.order_by.data,form.sort.data)
+            elif form.to_ent.data:
+                output.append('You searched for donations to ' + form.to_ent.data)
+                output.append('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
+                searchedcomms=Committees.get_all_to_entity_range(form.to_ent.data, form.from_year.data,form.to_year.data,form.order_by.data,form.sort.data)
+            elif form.from_ent.data: 
+                output.append('You searched for donations from ' + form.from_ent.data)
+                output.append('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
+                searchedcomms=Committees.get_all_from_entity_range(form.from_ent.data, form.from_year.data,form.to_year.data,form.order_by.data,form.sort.data)
+            else:
+                searchedcomms=Committees.get_all_range(form.from_year.data,form.to_year.data,form.order_by.data,form.sort.data)                
+        elif type_form=='all donations':
+            output.append('Date range: '+str(form.from_year.data)+' to ' + str(form.to_year.data))
+            searchedcomms=Committees.get_all_range(form.from_year.data,form.to_year.data,form.order_by.data,form.sort.data)
+        output.append('Sorted by: ' + form.order_by.data + ' ' + form.sort.data)
+        
+    return render_template('committees.html', form=form,type_form=type_form,all_committees=searchedcomms,messages=output)
+
+def helper(query,form):
+    return render_template('committees.html',form=form,type_form=query)
 
 
     
--- mini-amazon-skeleton/app/models/committees.py
+++ mini-amazon-skeleton/app/models/committees.py
@@ -23,104 +23,130 @@ WHERE cid = :cid
         return Committees(*(rows[0])) if rows is not None else None
 
     @staticmethod
-    def get_all(): #just gets everyting in the table
+    def get_all(sort_by, aord): #just gets everyting in the table
         rows = app.db.execute('''
 SELECT cid, from_entity, to_entity, donation_amount,  from_category, to_category, yr
-FROM Committees
+FROM Committees 
+ORDER BY CASE WHEN :AD='ascending' AND :s='ID' THEN cid END ASC,
+        CASE WHEN :AD='ascending' AND :s='year' THEN yr END ASC,
+        CASE WHEN :AD='ascending' AND :s='donation amount' THEN donation_amount END ASC,
+        CASE WHEN :AD='descending' AND :s='donation amount' THEN donation_amount END DESC,
+        CASE WHEN :AD='descending' AND :s='ID' THEN cid END DESC,
+        CASE WHEN :AD='descending' AND :s='year' THEN yr END DESC
+        
 ''',
+                              s=sort_by,
+                              AD=aord,
                               )
         return [Committees(*row) for row in rows]
 
     @staticmethod
-    def get_all_range(from_date, to_date): #gets everything in the table going to to_entity
+    def get_all_range(from_date, to_date, sort_by,aord): #gets everything in the table given a range of dates
         rows = app.db.execute('''
             SELECT cid, from_entity, to_entity, donation_amount, from_category, to_category, yr
             FROM Committees
             WHERE yr>=:y1 AND yr<=:y2
+            ORDER BY CASE WHEN :AD='ascending' AND :s='ID' THEN cid END ASC,
+        CASE WHEN :AD='ascending' AND :s='year' THEN yr END ASC,
+        CASE WHEN :AD='ascending' AND :s='donation amount' THEN donation_amount END ASC,
+        CASE WHEN :AD='descending' AND :s='donation amount' THEN donation_amount END DESC,
+        CASE WHEN :AD='descending' AND :s='ID' THEN cid END DESC,
+        CASE WHEN :AD='descending' AND :s='year' THEN yr END DESC
             ''',
                               
                               y1=from_date,
                               y2=to_date,
+                              s=sort_by,
+                              AD=aord,
                               )
         return [Committees(*row) for row in rows]
 
     @staticmethod
-    def get_all_from_to_range(to_entity, from_entity,from_date, to_date): #gets everything in the table going to to_entity
+    def get_all_from_to_range(to_entity, from_entity,from_date, to_date, sort_by,aord): #gets everything in the table going to to_entity and from from_entity
         rows = app.db.execute('''
             SELECT cid, from_entity, to_entity, donation_amount, from_category, to_category, yr
             FROM Committees
             WHERE to_entity = :to_entity AND from_entity=:from_entity 
             AND yr>=:y1 AND yr<=:y2
+           ORDER BY CASE WHEN :AD='ascending' AND :s='ID' THEN cid END ASC,
+        CASE WHEN :AD='ascending' AND :s='year' THEN yr END ASC,
+        CASE WHEN :AD='ascending' AND :s='donation amount' THEN donation_amount END ASC,
+        CASE WHEN :AD='descending' AND :s='donation amount' THEN donation_amount END DESC,
+        CASE WHEN :AD='descending' AND :s='ID' THEN cid END DESC,
+        CASE WHEN :AD='descending' AND :s='year' THEN yr END DESC
             ''',
                               to_entity=to_entity,
                               from_entity=from_entity,
                               y1=from_date,
                               y2=to_date,
+                              s=sort_by,
+                              AD=aord,
                               )
         return [Committees(*row) for row in rows]
 
     @staticmethod
-    def get_all_to_entity_range(to_entity, from_date, to_date): #gets everything in the table going to to_entity given a particular time period
+    def get_all_to_entity_range(to_entity, from_date, to_date, sort_by,aord): #gets everything in the table going to to_entity
         rows = app.db.execute('''
             SELECT cid, from_entity, to_entity, donation_amount,  from_category, to_category, yr
             FROM Committees
             WHERE to_entity = :to_entity
             AND yr>=:y1 AND yr<=:y2
+            ORDER BY CASE WHEN :AD='ascending' AND :s='ID' THEN cid END ASC,
+        CASE WHEN :AD='ascending' AND :s='year' THEN yr END ASC,
+        CASE WHEN :AD='ascending' AND :s='donation amount' THEN donation_amount END ASC,
+        CASE WHEN :AD='descending' AND :s='donation amount' THEN donation_amount END DESC,
+        CASE WHEN :AD='descending' AND :s='ID' THEN cid END DESC,
+        CASE WHEN :AD='descending' AND :s='year' THEN yr END DESC
             ''',
                               to_entity=to_entity,
                               from_date=from_date,
                               y1=from_date,
                               y2=to_date,
+                              s=sort_by,
+                              AD=aord,
                               )
         return [Committees(*row) for row in rows]
     @staticmethod
-    def get_all_from_entity_range(from_entity, from_date, to_date): #gets everything in the table going from from_entity given a particular time period
+    def get_all_from_entity_range(from_entity, from_date, to_date, sort_by,aord): #gets everything in the table going from from_entity
         rows = app.db.execute('''
             SELECT cid, from_entity, to_entity, donation_amount,  from_category, to_category, yr
             FROM Committees
             WHERE from_entity = :from_entity
             AND yr>=:y1 AND yr<=:y2
+            ORDER BY CASE WHEN :AD='ascending' AND :s='ID' THEN cid END ASC,
+        CASE WHEN :AD='ascending' AND :s='year' THEN yr END ASC,
+        CASE WHEN :AD='ascending' AND :s='donation amount' THEN donation_amount END ASC,
+        CASE WHEN :AD='descending' AND :s='donation amount' THEN donation_amount END DESC,
+        CASE WHEN :AD='descending' AND :s='ID' THEN cid END DESC,
+        CASE WHEN :AD='descending' AND :s='year' THEN yr END DESC
             ''',
                               from_entity=from_entity,
                               y1=from_date,
-                              y2=to_date
+                              y2=to_date,
+                              s=sort_by,
+                              AD=aord,
                               )
         return [Committees(*row) for row in rows]
+    
     @staticmethod
-    def sort_by_cid():
-        rows = app.db.execute('''
-            SELECT *
-            FROM Committees
-            ORDER BY cid ASC
-            ''',
-                              )
-        return [Committees(*row) for row in rows]
-    @staticmethod
-    def sort_by_date_asc():
-        rows = app.db.execute('''
-            SELECT *
-            FROM Committees
-            ORDER BY yr ASC
-            ''',
-                              )
-        return [Committees(*row) for row in rows]
-    @staticmethod
-    def sort_by_date_desc():
-        rows = app.db.execute('''
-            SELECT *
-            FROM Committees
-            ORDER BY yr DESC
-            ''',
-                              )
-        return [Committees(*row) for row in rows]
-    @staticmethod
-    def get_all_involving(entity):
+    def get_all_involving(search_entity,from_date,to_date, sort_by,aord):
         rows = app.db.execute('''
             SELECT cid, from_entity, to_entity, donation_amount,  from_category, to_category, yr
             FROM Committees
             WHERE from_entity = :entity 
-            OR to_entity=:entity
+            OR to_entity=:entity AND yr>=:y1 AND yr<=:y2
+           ORDER BY CASE WHEN :AD='ascending' AND :s='ID' THEN cid END ASC,
+        CASE WHEN :AD='ascending' AND :s='year' THEN yr END ASC,
+        CASE WHEN :AD='ascending' AND :s='donation amount' THEN donation_amount END ASC,
+        CASE WHEN :AD='descending' AND :s='donation amount' THEN donation_amount END DESC,
+        CASE WHEN :AD='descending' AND :s='ID' THEN cid END DESC,
+        CASE WHEN :AD='descending' AND :s='year' THEN yr END DESC
             ''',
-                              entity=entity
+                              entity=search_entity,
+                              y1=from_date,
+                              y2=to_date,
+                              s=sort_by,
+                              AD=aord,
                               )
         return [Committees(*row) for row in rows]
+       
